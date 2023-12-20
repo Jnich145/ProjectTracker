14a6043c428a924a387e2222012c486d493328db
Justin <invicitusreshat@outlook.com>
2023-12-20 09:13:21 -0600

    Feature 14 complete

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from projects.models import Project
 from django.contrib.auth.decorators import login_required
+from projects.forms import ProjectForm
 
 # Create your views here.
 @login_required
@@ -10,3 +11,21 @@ def list_projects(request):
         "list_projects": list_projects,
     }
     return render(request, "projects/list.html", context)
+
+
+@login_required
+def create_project(request):
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            project = form.save(False)
+            project.author = request.user
+            project.save()
+            return redirect("list_projects")
+    else:
+        form = ProjectForm()
+    
+    context = {
+        "form": form,
+    }
+    return render(request, 'projects/create.html', context)
